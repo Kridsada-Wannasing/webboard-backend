ca6da7792b7e877e2134cbad002aca3c916e2569
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-18 23:02:27 +0700

    feature register

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,6 +16,14 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
 });
 
 userSchema.methods.lastLoginAt = () => {

ce5c81b8872406e10218c45bcc0a249e7bea27bf
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-18 23:51:58 +0700

    parser json and urlencoded

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const userRoutes = require("./routes/User");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }));
+
 app.use("/api/v1/user", userRoutes);
 
 module.exports = app;

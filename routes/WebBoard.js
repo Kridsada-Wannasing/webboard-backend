69584b58be06ee4d16731444b640ef74f00ef3d2
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-19 08:15:43 +0700

    edit web-board-route

--- routes/WebBoard.js
+++ routes/WebBoard.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const webBoardControllers = require("../controllers/WebBoard");
 
 router.post("/", webBoardControllers.createWebBoard);
+router.patch("/:id", webBoardControllers.editWebBoard);
 
 module.exports = router;

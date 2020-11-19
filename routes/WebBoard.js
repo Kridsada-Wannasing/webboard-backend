f2491295d9f9dc7a54c8cb0415cb270c98d68de8
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-19 08:16:58 +0700

    get web-board route

--- routes/WebBoard.js
+++ routes/WebBoard.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const webBoardControllers = require("../controllers/WebBoard");
 
+router.get("/", webBoardControllers.getAllWebBoard);
+router.get("/:id", webBoardControllers.getWebBoardById);
 router.post("/", webBoardControllers.createWebBoard);
 
 module.exports = router;
